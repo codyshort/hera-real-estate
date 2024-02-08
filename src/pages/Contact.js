0a591ea7aea98a1606815ca8682cc81e0d23583e
codyshort <ccshort1@gmail.com>
2024-02-07 22:14:44 -0500

    ReacatJS begin

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+function Contact() {
+    return(
+        <div>
+            <h2>Contact</h2>
+            <p>This is the contact section.</p>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/pages/Properties.js
@@ -0,0 +1,10 @@
+function Properties() {
+    return(
+        <div>
+            <h2>Properties</h2>
+            <p>This is the properties section.</p>
+        </div>
+    )
+}
+
+export default Properties;
